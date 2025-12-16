1a27d2a593916014f63ba02ab79dd637522fdf0c
Sohail Khan <skhan863601@gmail.com>
2025-12-16 20:01:21 +0530

    secound commi

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Sohail")
+let name = "Sohail Khan";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+const city = "Bhiwandi";
+
+city = "pune"
+console.log(city);
+
+console.log(city);
