5767e6cef4dbf82fcae5c98b658cb523b7776bb2
Sohail Khan <skhan863601@gmail.com>
2025-12-17 19:09:24 +0530

    lesson 4 completed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// =======================
 // Lesson 1: console.log()
 // console.log ka use output dikhane ke liye hota hai
 // =======================
 console.log("Sohail");
 
 
-// =======================
 // Lesson 2: let
 // let ki value ko hum baad me change kar sakte hain
 // =======================
@@ -15,11 +13,46 @@ let age = 20;
 console.log(name);
 console.log(age);
 
+name = "Sohail";
+age = 21;    // value change (let allow karta hai)
 
-// =======================
+console.log(name);
+console.log(age);
+
+
 // Lesson 3: const
 // const ki value ko hum change nahi kar sakte
 // =======================
 const city = "Bhiwandi";
-
 console.log(city);
+
+
+// Lesson 4: Data Type - String
+// String hamesha quotes me hoti hai "" ya ''
+// =======================
+let course = "JavaScript";
+console.log(course);
+
+// string change
+course = "React";
+console.log(course);
+
+// Lesson 4: Data Type - Number
+// Number ka matlab hota hai digits
+// =======================
+let price = 99.5;
+console.log(price);
+
+// number change
+price = 120;
+console.log(price);
+
+
+// Lesson 4: Data Type - Boolean
+// Boolean me sirf true ya false hota hai
+let isStudent = true;
+let isAdult = false;
+
+console.log(isStudent);
+console.log(isAdult);
+
