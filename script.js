d7b035769bbe051042fdd2809a103637c904278c
Sohail Khan <skhan863601@gmail.com>
2025-12-17 16:00:20 +0530

    Lesson 3 completed

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// =======================
+// Lesson 1: console.log()
+// console.log ka use output dikhane ke liye hota hai
+// =======================
+console.log("Sohail");
+
+
+// =======================
+// Lesson 2: let
+// let ki value ko hum baad me change kar sakte hain
+// =======================
 let name = "Sohail Khan";
 let age = 20;
 
 console.log(name);
 console.log(age);
 
-const city = "Bhiwandi";
 
-city = "pune"
-console.log(city);
+// =======================
+// Lesson 3: const
+// const ki value ko hum change nahi kar sakte
+// =======================
+const city = "Bhiwandi";
 
 console.log(city);
